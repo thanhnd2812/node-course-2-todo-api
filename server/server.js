746d4e4037c35a1f5b99150e5ccbd21a7f615014
njt28 <thanh.nguyen@zorpiahq.com>
2018-05-02 12:35:58 +0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -56,6 +56,27 @@ app.get('/todos/:id', (req, res) => {
   );
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then(
+    todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    },
+    err => {
+      res.send(err);
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log('Started on port 3000');
 });
