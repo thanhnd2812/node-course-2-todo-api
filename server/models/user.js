b6ea661955f36071e0d5b852f55bb78f4e58f5fc
njt28 <thanh.nguyen@zorpiahq.com>
2018-05-04 10:39:36 +0700

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -63,6 +63,23 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (error) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var config = require('./config/config');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -126,6 +127,10 @@ app.post('/users', (req, res) => {
       res.header('x-auth', token).send(user);
     })
     .catch(e => res.status(400).send(e));
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
