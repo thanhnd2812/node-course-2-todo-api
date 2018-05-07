129415b8e061b272f242a3c598d1b1b99d4c7326
njt28 <thanh.nguyen@zorpiahq.com>
2018-05-07 12:10:10 +0700

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -53,7 +53,7 @@ UserSchema.methods.generateAuthToken = function() {
         _id: user._id.toHexString(),
         access
       },
-      'abc123'
+      process.env.JWT_SECRET
     )
     .toString();
 
@@ -80,7 +80,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (error) {
     return Promise.reject();
   }
